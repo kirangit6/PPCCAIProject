af78dbb9878930b2b38b5d3f3ef6a4411d4fd6c8
kirangit6 <keerankumari6@gmail.com>
2024-08-05 15:16:26 +0530

    Add files via upload

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -295,152 +295,160 @@ const handleSubmitSheet = async (e) => {
 
         <section className="frame-image-sc bg-black space-mr">
           <AnimatedSection>
-            <div className="wrapper">
-              <div className="desktop_mockup">
-                <div className="google_interface">
-                  <div className="google_serach">
-                    <input type="text" className="google_serach_input" />
-                  </div>
-                  <div className="google_serach_result">
-                    <div className="serach_item">
-                      <div className="brand_logo_img">
-                        <img src="images/download.png" alt="" />
-                      </div>
-                      <div className="brand_info">
-                        <div className="barnd_name">
-                          <h6>GetMunch</h6>
-                          <div className="link">
-                            <a
-                              href="https://www.getmunch.com"
-                              target="_blank"
-                              className="website_link"
-                            >
-                              https://www.getmunch.com
-                            </a>
-                            <a href="" className="modal_dot">
-                              <i className="bx bx-dots-vertical-rounded"></i>
-                            </a>
+            <div className="container">
+            <div class="wrapper">
+              <div class="desktop_mockup">
+                <div class="google_interface">
+                  <div class="header_area">
+                    <div class="header_ser">
+                      <div class="serach_wrapper">
+                        <div class="google_logo">
+                          <img src="images/Google_2015_logo.svg.png" alt="" height="40px"/>
+                        </div>
+                        <div class="google_serach">
+                          <div class="ser">
+                            <input type="text" class="google_serach_input"/>
+                              <div class="close d-none" >
+                                <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
+                              </div>
                           </div>
+                          <div class="end_area">
+                            <button class=" goog_btn mic">
+                              <img src="images/Google_mic.svg" alt=""/>
+                            </button>
+                            <button class=" goog_btn lens">
+                              <img src="images/7123028_lens_google_icon.svg" alt=""/>
+                            </button>
+                            <button class=" goog_btn serch_ico">
+                              <img src="images/Vector_search_icon.svg.png" alt=""/>
+                            </button>
+                          </div>
+
+                        </div>
+                      </div>
+                      <div class="right ">
+                        <div class="search_labs">
+                          <img src="images/serachlabs.png" alt="" width="25px"/>
+                        </div>
+                        <div class="google_apps">
+                          <img src="images/186401_grid_icon.png" alt="" width="25px"/>
+
+                        </div>
+                        <div class="gmail">
+                          <img src="images/gamil.png" alt=""/>
                         </div>
                       </div>
                     </div>
-                    <div className="serach_result_main">
-                      <a
-                        href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujnDj13SG5uIE0xTl617SuBP3vYSLCBm1vr0jMR6uF-aQm83rxWE5xRoCpd0QAvD_BwE"
-                        className=""
-                        target="_blank"
-                      >
-                        <h1>
-                          {" "}
-                          GetMunch - AI Video Editing | Munch App - Register Now
-                        </h1>
+                    <div class="tabs">
+                      <div class="left_tabs">
+                        <button class="tab_btn active">All</button>
+                        <button class="tab_btn">Images</button>
+                        <button class="tab_btn">Videos</button>
+                        <button class="tab_btn">News</button>
+                        <button class="tab_btn">Shopping</button>
+                        <button class="tab_btn">Maps</button>
+                        <button class="tab_btn">Books</button>
+                        <button class="tab_btn d-none"> <span><i class='bx bx-dots-vertical-rounded'></i></span> More</button>
+                      </div>
+                      <div class="right_tabs d-none">
+                        <button class="tab_btn ">Tool</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="capsul_tab">
+                    <button class="capsul">Free</button>
+                    <button class="capsul">Reviews</button>
+                    <button class="capsul">Open Now</button>
+                    <button class="capsul">Clips</button>
+                    <button class="capsul">Pricing</button>
+                    <button class="capsul">Alternative</button>
+                  </div>
+                  <div class="google_serach_result">
+                    <div class="serach_item">
+                      <div class="brand_logo_img">
+                        <img src="images/download.png" alt=""/>
+                      </div>
+                      <div class="brand_info">
+                        <div class="barnd_name">
+                          <h6>GetMunch</h6>
+                          <div class="link">
+                            <a href="https://www.getmunch.com" target="_blank" class="website_link">https://www.getmunch.com</a>
+                            <a href="" class="modal_dot"><i class='bx bx-dots-vertical-rounded' ></i></a>
+                          </div>
+
+                        </div>
+                      </div>
+                    </div>
+                    <div class="serach_result_main">
+
+                      <a href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujnDj13SG5uIE0xTl617SuBP3vYSLCBm1vr0jMR6uF-aQm83rxWE5xRoCpd0QAvD_BwE" class="" target="_blank">
+                        <h1>  GetMunch - AI Video Editing | Munch App - Register Now</h1>
                       </a>
 
-                      <p className="info">
-                        <span className="brand_name_highlisght">Munch</span>{" "}
-                        extracts the most engaging, trending and impactful clips
-                        from your long-form videos.
-                        <span className="brand_name_highlisght">Munch</span> is
-                        centered around machine learning capabilities, designed
-                        to keep what's important.
-                      </p>
+                      <p class="info"><span class="brand_name_highlisght">Munch</span> extracts the most engaging, trending and impactful clips from your long-form videos.
+                        <span class="brand_name_highlisght">Munch</span> is centered around machine learning capabilities, designed to keep what's important.</p>
                     </div>
-                    <div className="Ktlw8e">
-                      <div className="iCzAIb">
-                        <div className="d-flx">
-                          <a
-                            href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE"
-                            className=""
-                          >
+                    <div class="Ktlw8e">
+                      <div class="iCzAIb">
+                        <div class="d-flx">
+                          <a href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE" class="">
                             GetMunch Pricing
                           </a>
-                          <div className="icon d-none-icon">
-                            <i className="bx bx-chevron-right"></i>
-                          </div>
+                          <div class="icon d-none-icon"><i class='bx bx-chevron-right'></i></div>
                         </div>
-                        <p className="info d-none">
-                          Check Out The Pricing And Pick The Best Plan
-                        </p>
+                        <p class="info d-none">Check Out The Pricing And Pick The Best Plan</p>
                       </div>
                     </div>
-                    <div className="Ktlw8e">
-                      <div className="iCzAIb">
-                        <div className="d-flx">
-                          <a
-                            href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE"
-                            className=""
-                          >
+                    <div class="Ktlw8e">
+                      <div class="iCzAIb">
+                        <div class="d-flx">
+                          <a href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE" class="">
                             Login
                           </a>
-                          <div className="icon d-none-icon">
-                            <i className="bx bx-chevron-right"></i>
-                          </div>
+                          <div class="icon d-none-icon"><i class='bx bx-chevron-right'></i></div>
                         </div>
-                        <p className="info d-none">
-                          Enter the Required Details To Log In To Your Account.
-                        </p>
+                        <p class="info d-none">Enter the Required Details To Log In To Your Account.</p>
                       </div>
                     </div>
-                    <div className="Ktlw8e">
-                      <div className="iCzAIb">
-                        <div className="d-flx">
-                          <a
-                            href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE"
-                            className=""
-                          >
+                    <div class="Ktlw8e">
+                      <div class="iCzAIb">
+                        <div class="d-flx">
+                          <a href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE" class="">
                             Main Page
                           </a>
-                          <div className="icon d-none-icon">
-                            <i className="bx bx-chevron-right"></i>
-                          </div>
+                          <div class="icon d-none-icon"><i class='bx bx-chevron-right'></i></div>
                         </div>
-                        <p className="info d-none">
-                          Get To Know Us And Find Out More
-                        </p>
+                        <p class="info d-none">Get To Know Us And Find Out More</p>
                       </div>
                     </div>
-                    <div className="Ktlw8e">
-                      <div className="iCzAIb">
-                        <div className="d-flx">
-                          <a
-                            href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE"
-                            className=""
-                          >
+                    <div class="Ktlw8e">
+                      <div class="iCzAIb">
+                        <div class="d-flx">
+                          <a href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE" class="">
                             Become An Affiliate
                           </a>
-                          <div className="icon d-none-icon">
-                            <i className="bx bx-chevron-right"></i>
-                          </div>
+                          <div class="icon d-none-icon"><i class='bx bx-chevron-right'></i></div>
                         </div>
 
-                        <p className="info d-none">
-                          Fill Out the Form With Your Details To Join Our
-                          Program.
-                        </p>
+                        <p class="info d-none">Fill Out the Form With Your Details To Join Our Program.</p>
                       </div>
                     </div>
-                    <div className="Ktlw8e">
-                      <div className="iCzAIb">
-                        <div className="d-flx">
-                          <a
-                            href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE"
-                            className=""
-                          >
+                    <div class="Ktlw8e">
+                      <div class="iCzAIb">
+                        <div class="d-flx">
+                          <a href="https://www.getmunch.com/?everid=&oid=2&affid=45&source_id=munch-pricing&gad_source=1&gclid=CjwKCAjw5Ky1BhAgEiwA5jGujvEvl46e6lRPJumAVHq3s3IGvPwjOdtBDxZykDHpcU9tw1d7DbP84xoCu_QQAvD_BwE" class="">
                             Our Blog
                           </a>
-                          <div className="icon d-none-icon">
-                            <i className="bx bx-chevron-right"></i>
-                          </div>
+                          <div class="icon d-none-icon"><i class='bx bx-chevron-right'></i></div>
                         </div>
 
-                        <p className="info d-none">
-                          Check Out Our Blog And Get Inspired
-                        </p>
+                        <p class="info d-none">Check Out Our Blog And Get Inspired</p>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
+            </div>
             </div>
           </AnimatedSection>
         </section>
